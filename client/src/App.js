441289e781b53a22b035f5c90c81f9e7b7f5be96
baqir <baqiresfandiari1996@gmail.com>
2022-04-28 11:37:34 +0430

    Part1_Create_react_app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
+import Nav from './screen/Nav';
+function App() {
+  return (
+    <div>
+      <Nav />
+      <h6>List of Product</h6>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/screen/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Nav() {
+  return (
+    <div>
+      <nav class="navbar bNav navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <p class="navbar-brand" href="#">
+            Amazona
+          </p>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-lg-0"></ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
